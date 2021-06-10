4ec4107ef28b0bfedb5060e59f4f34f58927a0f3
Catalin Tugui <katamaxx23@gmail.com>
2021-06-10 12:54:06 +0300

    files

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+    setTimeout(function(){
+        key.classList.remove('playing');
+    }, 100)
+}
+
+window.addEventListener('keydown', playSound);
+
+
+    
